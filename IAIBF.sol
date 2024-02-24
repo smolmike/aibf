7ad7662d23b9f74382a2fdd2ba2c10a48532d284
smolmike <68653554+smolmike@users.noreply.github.com>
2024-02-24 17:02:44 -0500

    Update IAIBF.sol

--- IAIBF.sol
+++ IAIBF.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.16;
+pragma solidity 0.8.20;
 
 interface IAIBF {
     function balanceOf(address owner) external view returns (uint256 balance);
