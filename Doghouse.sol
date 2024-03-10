7512071606c855a2b66d7e4dd64a61678f2e5785
smolmike <68653554+smolmike@users.noreply.github.com>
2024-03-10 14:43:57 -0400

    Update Doghouse.sol

--- Doghouse.sol
+++ Doghouse.sol
@@ -3,6 +3,7 @@
 /*
 Welcome to the DOGHOUSE 8===8
 
+Created by @ITZMIZZLE
               _
             ,/A\,
           .//`_`\\,
@@ -20,27 +21,26 @@ Welcome to the DOGHOUSE 8===8
 
 */
 
-// SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.20;
 
-import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts@4.4.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.4.0/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts@4.4.0/security/Pausable.sol";
+import "@openzeppelin/contracts@4.4.0/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts@4.4.0/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts@4.4.0/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts@4.4.0/token/ERC20/utils/SafeERC20.sol";
 
-contract TheDogHouse is AccessControlEnumerable, IERC721Receiver, ReentrancyGuard {
+
+contract TheDogHouse is Ownable, IERC721Receiver, ReentrancyGuard, Pausable {
     using SafeERC20 for IERC20;
-    using EnumerableSet for EnumerableSet.UintSet;
 
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-    IERC721 public nft;
     IERC20 public rewardToken;
+    IERC721 public nftCollection;
 
-    uint256 public timeUnit;
-    uint256 public rewardsPerUnitTime;
+    uint256 public dailyEmission;
+    uint256 public totalRewardsDistributed;
 
     struct Stake {
         address owner;
@@ -48,57 +48,83 @@ contract TheDogHouse is AccessControlEnumerable, IERC721Receiver, ReentrancyGuar
     }
 
     mapping(uint256 => Stake) public vault;
-    EnumerableSet.UintSet private stakedTokens;
 
-    uint256 public rewardTokenBalance;
+    event ItemStaked(uint256 tokenId, address owner);
+    event ItemUnstaked(uint256 tokenId, address owner);
+    event RewardClaimed(address owner, uint256 amount);
 
-    // Events remain the same
+    constructor() {}
 
-    constructor(address _nftAddress, address _rewardTokenAddress, uint256 _timeUnit, uint256 _rewardsPerUnitTime) {
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        _setupRole(ADMIN_ROLE, _msgSender());
-        nft = IERC721(_nftAddress);
-        rewardToken = IERC20(_rewardTokenAddress);
-        timeUnit = _timeUnit;
-        rewardsPerUnitTime = _rewardsPerUnitTime;
-    }
-
-    // Modifier for role checking
-    modifier onlyAdmin() {
-        require(hasRole(ADMIN_ROLE, _msgSender()), "Caller is not an admin");
-        _;
-    }
-
-    // Staking, unstaking, and reward token management functions remain similar, with added role checks where necessary
-
-    function calculateReward(uint256 tokenId) public view returns (uint256 reward) {
-        Stake memory stakeData = vault[tokenId];
-        uint256 stakingDuration = block.timestamp - stakeData.stakedAt;
-        uint256 timeUnitsStaked = stakingDuration / timeUnit;
-        reward = timeUnitsStaked * rewardsPerUnitTime;
-    }
-
-    // Admin functions for updating contract parameters like NFT address, reward token address, etc.
-
-    function setNFTAddress(address _nftAddress) external onlyAdmin {
-        nft = IERC721(_nftAddress);
-    }
-
-    function setRewardTokenAddress(address _rewardTokenAddress) external onlyAdmin {
+    function setRewardToken(address _rewardTokenAddress) external onlyOwner {
         rewardToken = IERC20(_rewardTokenAddress);
     }
 
-    function setTimeUnit(uint256 _timeUnit) external onlyAdmin {
-        timeUnit = _timeUnit;
+    function setNftCollection(address _nftCollectionAddress) external onlyOwner {
+        nftCollection = IERC721(_nftCollectionAddress);
     }
 
-    function setRewardsPerUnitTime(uint256 _rewardsPerUnitTime) external onlyAdmin {
-        rewardsPerUnitTime = _rewardsPerUnitTime;
+    function setDailyEmission(uint256 _dailyEmission) external onlyOwner {
+        dailyEmission = _dailyEmission;
     }
 
-    // Helper and view functions remain similar
+    function stakeTokens(uint256[] calldata tokenIds) external whenNotPaused nonReentrant {
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            uint256 tokenId = tokenIds[i];
+            require(nftCollection.ownerOf(tokenId) == msg.sender, "Not the token owner");
+            nftCollection.transferFrom(msg.sender, address(this), tokenId);
+            vault[tokenId] = Stake(msg.sender, block.timestamp);
+            emit ItemStaked(tokenId, msg.sender);
+        }
+    }
+
+    function unstakeTokens(uint256[] calldata tokenIds) external nonReentrant {
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            uint256 tokenId = tokenIds[i];
+            require(vault[tokenId].owner == msg.sender, "Not the staker");
+            delete vault[tokenId];
+            nftCollection.transferFrom(address(this), msg.sender, tokenId);
+            emit ItemUnstaked(tokenId, msg.sender);
+        }
+    }
+
+    function claimRewards(uint256[] calldata tokenIds) external nonReentrant {
+        uint256 totalReward = 0;
+
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            uint256 tokenId = tokenIds[i];
+            Stake memory stake = vault[tokenId];
+            require(stake.owner == msg.sender, "Not the staker");
+
+            uint256 reward = calculateReward(stake.stakedAt);
+            totalReward += reward;
+
+            // Update the staked time to now
+            vault[tokenId].stakedAt = block.timestamp;
+        }
+
+        require(totalRewardsDistributed + totalReward <= rewardToken.balanceOf(address(this)), "Not enough rewards");
+        totalRewardsDistributed += totalReward;
+        rewardToken.safeTransfer(msg.sender, totalReward);
+
+        emit RewardClaimed(msg.sender, totalReward);
+    }
+
+    function calculateReward(uint256 stakedAt) public view returns (uint256) {
+        uint256 stakingDuration = block.timestamp - stakedAt;
+        uint256 reward = (dailyEmission * stakingDuration) / 1 days;
+        return reward;
+    }
 
     function onERC721Received(address, address, uint256, bytes memory) public virtual override returns (bytes4) {
         return this.onERC721Received.selector;
     }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
 }
+
